67fda699c9e6f9d0b5be2ed5e7254fcabb31b1fc
Akshey Saluja <akshay181@gmail.com>
2017-11-10 10:38:10 -0500

    Added New Features
    Added ability to grep keyword or IP Address to access.log
    Checked if program is running as root

--- whitelist_url_revised.sh
+++ whitelist_url_revised.sh
@@ -2,10 +2,7 @@
 
 : '
 Author Akshey Saluja
-'
-: '
-TODO:
-Make sure the each octet is 3 characters
+Company SEEPEX Inc.
 '
 
 dispalyOptions(){
@@ -13,8 +10,9 @@ dispalyOptions(){
 This will display all the options and the user will select the desired function.
 '
   echo "Please select what you would like to do:"
-  echo $'\nTo Whitelist a URL Enter: 1\nTo Whitelist an IP Address Enter: 2
-To Blacklist a URL Enter: 3\nTo Blacklist an IP Address Enter: 4\nTo Exclude a Word Enter: 5\n'
+  echo -e $'\n\033[1mEnter: 1\033[0m To Whitelist a URL \n\033[1m\033[1mEnter: 2\033[0m To Whitelist an IP Address
+\033[1mEnter: 3\033[0m To Blacklist a URL \n\033[1mEnter: 4\033[0m To Blacklist an IP Address \n\033[1mEnter: 5\033[0m To Exclude a Word
+\033[1mEnter: 6\033[0m To Find a Keyword or an IP Address in the access.log file\n'
 
 echo -n "Enter Option: "
 read option
@@ -25,28 +23,38 @@ executeOption(){
 : '
 This will select the user option.
 '
-if [ "$1" == "1" ]; then
-#Whitelist URL
-userInput 1
-elif [ "$1" == "2" ]; then
-#Whitelist IP
-userInput 2
-elif [ "$1" == "3" ]; then
-#Blacklist URL
-userInput 3
-elif [ "$1" == "4" ]; then
-#Blacklist IP
-userInput 4
-elif [ "$1" == "5" ]; then
-#Whitelist Regex
-userInput 5
-
-else
-    echo -e '\033[1m\nPlease follow Instructions!!!\033[0m'
-    echo -e '\033[1mEnter a number that is provided\n\033[0m'
+case $1 in
+  "1" )
+  #Whitelist URL
+  userInput 1
+    ;;
+    "2")
+    #Whitelist IP
+    userInput 2
+    ;;
+    "3")
+    #Blacklist URL
+    userInput 3
+    ;;
+    "4")
+    #Blacklist IP
+    userInput 4
+    ;;
+    "5")
+    #Whitelist Regex
+    userInput 5
+    ;;
+    "6")
+    #Read accesslog
+    userInput 6
+    ;;
+    *)
+    echo -e '\033[1m\nPlease follow Instructions!!!
+Enter a number that is provided\n\033[0m'
+echo -e '\033[1m\033[0m'
     dispalyOptions
-fi
-
+    ;;
+esac
 }
 
 userInput(){
@@ -58,35 +66,40 @@ This will give the proper input based on the option they choose.
 3== Blacklist URL
 4== Blacklist IP Address
 5== Whitelist Word in Regex
+6== Find Keyword or IP Address in access log
 '
-
-if [ "$1" == "1" ];
-then
+case $1 in
+  "1" )
   echo -n "Enter URL to whilelist: (Ex. URL(https://www.apple.com) Enter .apple.com) "
   read answer
   checkURLInput $answer 3
-elif [ "$1" == "2" ];
-then
-  echo -n "Enter IP Address to whilelist: (Ex. 172.217.20.78) "
-  read answer
-  checkIPInput $answer 3
-elif [ "$1" == "3" ];
-then
-  echo -n "Enter URL to blacklist: (Ex. URL(https://www.apple.com) Enter .apple.com) "
-  read answer
-  checkURLInput $answer 4
-elif [ "$1" == "4" ];
-then
-  echo -n "Enter IP Address to blacklist: (Ex. 172.217.20.78) "
-  read answer
-  checkIPInput $answer 4
-elif [ "$1" == "5" ];
-then
-  echo -n "Enter a word to exclude : (Ex. PipeNipples) "
-  read answer
-  addToRegex $answer
-fi
-
+    ;;
+    "2")
+    echo -n "Enter IP Address to whilelist: (Ex. 172.217.20.78) "
+    read answer
+    checkIPInput $answer 3
+    ;;
+    "3")
+    echo -n "Enter URL to blacklist: (Ex. URL(https://www.apple.com) Enter .apple.com) "
+    read answer
+    checkURLInput $answer 4
+    ;;
+    "4")
+    echo -n "Enter IP Address to blacklist: (Ex. 172.217.20.78) "
+    read answer
+    checkIPInput $answer 4
+    ;;
+    "5")
+    echo -n "Enter a word to exclude : (Ex. PipeNipples) "
+    read answer
+    addToRegex $answer
+    ;;
+    "6")
+    echo -n "Enter Keyword or IP Address to find in the access Logs: "
+    read answer
+    readAccessLog $answer
+    ;;
+esac
 }
 
 checkURLInput(){
@@ -121,11 +134,10 @@ if [ "$2" == "3" ]; then
      addToWhitelist $1 3
 
 elif [ "$2" == "4" ]; then
-    addToBlacklist $1 4
+    addToBlacklist $1 3
 fi
 
 }
-
 
 checkIPInput(){
 : '
@@ -134,13 +146,14 @@ $1 == IP
 $2 == Whitelist/Blacklist
 3 == Whitelist
 4 == Blacklist
+5 == grep IP Address
 
 '
 
 # Check to see if IP Address is valid
 if   [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     if [ "$2"=="4" ]; then
-       addToWhitelist $1 3
+       addToWhitelist $1 4
 
     elif [ "$2" == "4" ]; then
       addToBlacklist $1 4
@@ -184,7 +197,6 @@ $2 == option
 3 == URL
 4 == IP Address
 '
-
     if [ "$2" == "3" ]; then
       # URL
       echo $1 >> /etc/squid/squid-manual-blacklist.acl
@@ -196,7 +208,31 @@ $2 == option
 }
 
 addToRegex(){
+  : '
+  This will add the userInput to the appropriate file
+  $1 == Word
+  '
+  #Check to see if input is not empty
+  if [[ -z "$2" ]]; then
+    echo -e '\033[1mPlease follow Instructions!!!
+Your input not invalid
+Please try again\033[0m'
+    dispalyOptions
+    return
+  fi
+
 echo $1 >> /etc/squid/whitelist-regex.acl
+}
+
+readAccessLog(){
+: '
+This will allow the user to tail -f  $1 /var/log/squid/access.log
+$1 == Keyword or IP
+'
+tail -f /var/log/squid/access.log | grep $1 & # run in background
+read -sn 1 #wait for user input
+kill %1 #kill the first background progress, i.e. tail
+
 }
 
 reconfigureSquid(){
@@ -204,6 +240,13 @@ reconfigureSquid(){
   /usr/sbin/squid -k reconfigure
   echo "Done"
 }
+main(){
+# Check to make sure user is running as root
+if [[ "$(whoami)" != "root" ]]; then
+  echo -e '\033[1mPlease Run the Program with sudo
+i.e sudo ./whitelist_url.sh\033[0m'
+exit
+fi
 
 X=1
 while [ $X = 1 ]
@@ -219,3 +262,5 @@ done
 reconfigureSquid
 echo -e "Thank you for using Akshey Saluja's Squid Program"
 exit
+}
+main
